6ef741e10857d95c120558cdeebf53503d00426c
junnydpetra <junnycmenezes@gmail.com>
2024-02-23 11:44:54 -0300

    initial commit

---
+++ app/Database/Migrations/2024-02-23-144059_Clientes.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Clientes extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id'                 => [
+                'type'           => 'INT',
+                'constraint'     => 9,
+                'usigned'        => true,
+                'auto_increment' => true,
+            ],
+            
+            'nome'               => [
+                'type'           => 'VARCHAR',
+                'constraint'     => 128,
+            ],
+            
+            'data_de_nascimento'    => [
+                'type'  => 'DATE',
+            ],
+            
+            'CPF'   => [
+                'type'  => 'VARCHAR',
+                'constraint'    => 11,
+            ],
+            
+            'telefone'    => [
+                'type'      => 'VARCHAR',
+                'constraint'     => 11,
+            ],
+            
+            'endereco'    => [
+                'type'      => 'VARCHAR',
+                'constraint'     => 11,
+            ],
+            
+            'limite_de_credito'    => [
+                'type'      => 'DOUBLE',
+            ],
+            
+            'dia_de_pagamento'    => [
+                'type'      => 'INT',
+            ],
+             
+            'created_at'    => [
+                'type'      => 'DATETIME',
+            ],
+            
+            'updated_at'    => [
+                'type'      => 'DATETIME',
+            ],
+
+            'deleted_at'    => [
+                'type'  => 'DATETIME',
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('clientes');
+    }
+
+    public function down()
+    {
+        //
+    }
+}
