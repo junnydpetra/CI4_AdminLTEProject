efde7b0db59af46e84c4e5e0aab1caf56ecefbc8
junnydpetra <junnycmenezes@gmail.com>
2024-03-01 16:48:35 -0300

    widgets

--- app/Views/inicio/index.php
+++ app/Views/inicio/index.php
@@ -20,6 +20,39 @@
   
   <div class="content">
     <div class="container-fluid">
+        <div class="row">
+
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box">
+              <span class="info-box-icon bg-info"><i class="far fa-envelope"></i></span>
+              <div class="info-box-content">
+                <span class="info-box-text">Messages</span>
+                <span class="info-box-number">1,410</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box">
+              <span class="info-box-icon bg-success"><i class="far fa-flag"></i></span>
+              <div class="info-box-content">
+                <span class="info-box-text">Bookmarks</span>
+                <span class="info-box-number">410</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box">
+              <span class="info-box-icon bg-warning"><i class="far fa-copy"></i></span>
+              <div class="info-box-content">
+                <span class="info-box-text">Uploads</span>
+                <span class="info-box-number">13,648</span>
+              </div>
+            </div>
+          </div>
+
+        </div>
     </div>
   </div>
 
